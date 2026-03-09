ea171d96afd84eff3fc194b57e8fa2b11ef1a459
Zoey Zhang <zhangz3462@my.sduhsd.net>
2026-03-09 13:37:11 -0700

    m

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/canyon-crest/react-app-zhangz3462/',
+  base: '/react-app-zhangz3462/',
 })
